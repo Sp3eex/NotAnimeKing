0b52fd9dfe37a6df09f28eb2004c98adb362d431
AnimeJayKing <58985150+AnimeJayKing@users.noreply.github.com>
2020-05-17 02:32:50 -0500

    Update NotAnimeKing.py

--- NotAnimeKing.py
+++ NotAnimeKing.py
@@ -1,7 +1,6 @@
 import random
 import json
 from itertools import cycle
-import youtube_dl
 import discord
 from discord.ext import commands, tasks
 
@@ -136,5 +135,51 @@ async def prefix(ctx, prefix):
 
     with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
+
+
+@client.event
+async def on_member_join(member):
+    with open('money.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open('money.json', 'w') as f:
+        json.dump(users, f)
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot == False:
+        with open('money.json', 'r') as f:
+            users = json.load(f)
+
+        await update_data(users, message.author)
+        await add_experience(users, message.author, 5)
+        await level_up(users, message.author, message)
+
+        with open('money.json', 'w') as f:
+            json.dump(users, f)
+    await client.process_commands(message)
+
+
+async def update_data(users, user):
+    if not f'{user.id}' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['experience'] = 0
+        users[f'{user.id}']['level'] = 1
+
+
+async def add_experience(users, user, exp):
+    users[f'{user.id}']['experience'] += exp
+
+
+async def level_up(users, user, message):
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1 / 4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
 
 client.run('NzA5NTY0MTYyNzI0MDY5NDA2.Xro6fg.SIXANc6rqjYy_iBCR_FW3a1rdwM')
